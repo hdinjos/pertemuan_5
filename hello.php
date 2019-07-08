44fa2180067ce50d147cc6fffe83158262cc8004
hendi saputra <hdinjos@gmail.com>
2019-07-09 02:08:12 +0700

    membuat website buku di 0-book

---
+++ hello.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>belajar php</title>
+	</head>
+	<body>
+		<h2>Sedang belajar php</h2>
+		<?php
+			echo "<h2>hello world</h2>";
+		?>
+		<!--atau-->
+		<h2><?php echo "hello world"; ?></h2>
+	</body>
+</html>
